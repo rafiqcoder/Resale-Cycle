0550921aed92a3823b26b21bd690a34725b9a6b2
rafiqcoder <rafiqcoder@gmail.com>
2022-11-24 13:06:39 +0600

    added header footer context and firebase

---
+++ src/Routes/Router.js
@@ -0,0 +1,50 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+
+import Home from "../Pages/Home/Home/Home";
+import NotFound from "../Pages/NotFound/NotFound";
+
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',element: <Main></Main>,
+        children: [
+            {
+                path: '/',element: <Home></Home>,
+            },
+
+
+
+            // {
+            //     path: '/contact-us',element: <ContactUs />
+            // },
+
+            // {
+            //     path: '/login',element: <Login />
+            // },
+            // {
+            //     path: '/register',element: <Register></Register>
+            // },
+
+            {
+                path: '*',element: <NotFound />
+            },
+        ]
+    },
+    // {
+    //     path: '/',element: <PrivateRoute><DashboardLayout /></PrivateRoute>,
+    //     children: [
+    //         {
+    //             path: '/dashboard',element: <Dashboard />,
+    //         },
+
+    //     ],
+    // },
+    {
+        path: '*',element: <NotFound />
+    },
+])
+
+
+export default router;
